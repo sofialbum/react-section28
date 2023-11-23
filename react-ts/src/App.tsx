b1c2ae1b4ba86c58cc3c2b6447cf6c8515d86ce6
Sofía González <sofia_1.6@hotmail.com>
2023-11-22 23:58:35 -0300

    updatesss

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,3 +1,4 @@
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
@@ -7,9 +8,14 @@ function App() {
     new Todo('Learn TypeScript'),
   ];
 
+  const addTodoHandler = (todoText: string) => {
+
+  }
+
   return (
     <div>
-      <Todos items={todos} />
+      <NewTodo onAddTodo={addTodoHandler}/>
+      <Todos items={todos} /> 
     </div>
   );
 }
